ae81c251f13da4c2f6dae6848e35d53f4cad350b
mrb1ngus <137521933+mrb1ngus@users.noreply.github.com>
2023-12-31 03:26:45 -0500

    Cambio 2 Leonardo Orozco DOCENTE

--- src/vista/frmDocente.java
+++ src/vista/frmDocente.java
@@ -5,6 +5,7 @@
 package vista;
 
 import controlador.clases.DocenteControl;
+import controlador.utiles.Utiles;
 import javax.swing.JOptionPane;
 import vista.modeloTablas.DocenteModeloTabla;
 
@@ -16,21 +17,21 @@ public class frmDocente extends javax.swing.JFrame {
 
     private DocenteControl docenteControl = new DocenteControl();
     private DocenteModeloTabla modelo = new DocenteModeloTabla();
-    
-    private Boolean verificar(){
-        return (    !(txtApellido.getText().trim().isEmpty())   &&
-                !(txtNombre.getText().trim().isEmpty())         &&
-                !(txtDni.getText().trim().isEmpty())            &&
-                !(txtTitulo.getText().trim().isEmpty())         );
+
+    private Boolean verificar() {
+        return (!(txtApellido.getText().trim().isEmpty())
+                && !(txtNombre.getText().trim().isEmpty())
+                && !(txtDni.getText().trim().isEmpty())
+                && !(txtTitulo.getText().trim().isEmpty()));
     }
 
-    public void cargarTabla(){
+    public void cargarTabla() {
         modelo.setDocentes(docenteControl.getListaDocentes());
         tblDocente.setModel(modelo);
         tblDocente.updateUI();
     }
-    
-    private void limpiar(){
+
+    private void limpiar() {
         txtApellido.setText("");
         txtBusqueda.setText("");
         txtDni.setText("");
@@ -39,53 +40,90 @@ public class frmDocente extends javax.swing.JFrame {
         tblDocente.clearSelection();
         cargarTabla();
     }
-    
-    private void guardar(){
-        if(verificar()){
+
+    private void guardar() {
+        if (verificar()) {
             docenteControl.getDocente().setNombre(txtNombre.getText());
             docenteControl.getDocente().setApellido(txtApellido.getText());
             docenteControl.getDocente().setDni(txtDni.getText());
             docenteControl.getDocente().setTitulo(txtTitulo.getText());
-            
-            if(docenteControl.persist()){
+
+            if (docenteControl.persist()) {
                 JOptionPane.showMessageDialog(null, "Guardado Exitoso");
-            }else{
+            } else {
                 JOptionPane.showMessageDialog(null, "No se pudo guardar");
             }
             docenteControl.setDocente(null);
             limpiar();
         }
     }
-    
-    private void modificar(){
-        if(verificar()){
+
+    private void modificar() {
+        if (verificar()) {
             docenteControl.getDocente().setNombre(txtNombre.getText());
             docenteControl.getDocente().setApellido(txtApellido.getText());
             docenteControl.getDocente().setDni(txtDni.getText());
             docenteControl.getDocente().setTitulo(txtTitulo.getText());
-            
-            if(docenteControl.merge(docenteControl.getDocente(), tblDocente.getSelectedRow())){
-                JOptionPane.showMessageDialog(null, "Modificacion Exitosa");
-            }else{
-                JOptionPane.showMessageDialog(null, "No se pudo modificar");
+
+            try {
+                Integer indiceDocente = Utiles.encontrarPosicion("docente", modelo.getDocentes().getInfo(tblDocente.getSelectedRow()).getId());
+
+                if (docenteControl.merge(docenteControl.getDocente(), indiceDocente)) {
+                    JOptionPane.showMessageDialog(null, "Modificacion Exitosa");
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se pudo modificar");
+                }
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
             }
+
             docenteControl.setDocente(null);
             limpiar();
         }
-    }    
-    
-    private void borrar(){
-        if(tblDocente.getSelectedRow() > -1){
-            if(docenteControl.remove(tblDocente.getSelectedRow())){
-                JOptionPane.showMessageDialog(null, "Se borro el elemento");
-            }else{
-                JOptionPane.showMessageDialog(null, "No se pudo borrar el elemento");            
+    }
+
+    private void borrar() {
+        try {
+            Integer indiceDocente = Utiles.encontrarPosicion("docente", modelo.getDocentes().getInfo(tblDocente.getSelectedRow()).getId());
+
+            if (tblDocente.getSelectedRow() > -1) {
+                if (docenteControl.remove(indiceDocente)) {
+                    JOptionPane.showMessageDialog(null, "Se borro el elemento");
+                } else {
+                    JOptionPane.showMessageDialog(null, "No se pudo borrar el elemento");
+                }
+            } else {
+                JOptionPane.showMessageDialog(null, "Seleccione un elemento para eliminar");
             }
-        }else{ 
-            JOptionPane.showMessageDialog(null, "Seleccione un elemento para eliminar");
+        } catch (Exception e) {
         }
         limpiar();
     }
+
+    private void ordenar() {
+        int t = 0;
+        if (btnTipoOrden.isSelected()) {
+            t = 1;
+        }
+        try {
+            modelo.setDocentes(docenteControl.shellsort(docenteControl.getListaDocentes(), t, cbxCriterioOrden.getSelectedItem().toString().toLowerCase()));
+        } catch (Exception e) {
+            System.out.println("Error al ordenar " + e.getMessage() + "");
+        }
+        tblDocente.setModel(modelo);
+        tblDocente.updateUI();
+    }
+
+    private void buscar() {
+        try {
+            modelo.setDocentes(docenteControl.busquedaLineal(txtBusqueda.getText(), docenteControl.getListaDocentes(), cbxCriterioBusqueda.getSelectedItem().toString().toLowerCase()));
+        } catch (Exception e) {
+            System.out.println("Error al buscar " + e.getMessage() + "");
+        }
+        tblDocente.setModel(modelo);
+        tblDocente.updateUI();
+    }
+
     /**
      * Creates new form frmDocente
      */
@@ -125,13 +163,17 @@ public class frmDocente extends javax.swing.JFrame {
         jLabel9 = new javax.swing.JLabel();
         cbxCriterioBusqueda = new javax.swing.JComboBox<>();
         cbxCriterioOrden = new javax.swing.JComboBox<>();
-        jButton5 = new javax.swing.JButton();
-        jButton6 = new javax.swing.JButton();
+        btnOrdenar = new javax.swing.JButton();
         txtBusqueda = new javax.swing.JTextField();
+        btnTipoOrden = new javax.swing.JRadioButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(465, 570));
+        setPreferredSize(new java.awt.Dimension(455, 580));
 
         jPanel1.setBorder(javax.swing.BorderFactory.createLineBorder(new java.awt.Color(0, 0, 0)));
+        jPanel1.setMinimumSize(new java.awt.Dimension(455, 580));
+        jPanel1.setPreferredSize(new java.awt.Dimension(465, 570));
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
         jLabel1.setFont(new java.awt.Font("Arial Black", 0, 14)); // NOI18N
@@ -234,18 +276,30 @@ public class frmDocente extends javax.swing.JFrame {
         jLabel9.setText("Criterio:");
         jPanel1.add(jLabel9, new org.netbeans.lib.awtextra.AbsoluteConstraints(30, 300, -1, -1));
 
-        cbxCriterioBusqueda.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbxCriterioBusqueda.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nombre", "Apellido", "Titulo", "DNI", "ID" }));
         jPanel1.add(cbxCriterioBusqueda, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 330, 110, -1));
 
-        cbxCriterioOrden.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Item 1", "Item 2", "Item 3", "Item 4" }));
+        cbxCriterioOrden.setModel(new javax.swing.DefaultComboBoxModel<>(new String[] { "Nombre", "Apellido", "Titulo", "DNI", "ID" }));
         jPanel1.add(cbxCriterioOrden, new org.netbeans.lib.awtextra.AbsoluteConstraints(80, 300, 110, -1));
 
-        jButton5.setText("ORDENAR");
-        jPanel1.add(jButton5, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 300, -1, -1));
+        btnOrdenar.setText("ORDENAR");
+        btnOrdenar.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnOrdenarActionPerformed(evt);
+            }
+        });
+        jPanel1.add(btnOrdenar, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 300, -1, -1));
 
-        jButton6.setText("BUSCAR");
-        jPanel1.add(jButton6, new org.netbeans.lib.awtextra.AbsoluteConstraints(360, 330, -1, -1));
-        jPanel1.add(txtBusqueda, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 330, 150, -1));
+        txtBusqueda.addKeyListener(new java.awt.event.KeyAdapter() {
+            public void keyPressed(java.awt.event.KeyEvent evt) {
+                txtBusquedaKeyPressed(evt);
+            }
+        });
+        jPanel1.add(txtBusqueda, new org.netbeans.lib.awtextra.AbsoluteConstraints(200, 330, 240, -1));
+
+        btnTipoOrden.setFont(new java.awt.Font("Segoe UI", 0, 11)); // NOI18N
+        btnTipoOrden.setText("Descendente");
+        jPanel1.add(btnTipoOrden, new org.netbeans.lib.awtextra.AbsoluteConstraints(290, 300, -1, 20));
 
         getContentPane().add(jPanel1, java.awt.BorderLayout.CENTER);
 
@@ -297,15 +351,32 @@ public class frmDocente extends javax.swing.JFrame {
     private void tblDocenteMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_tblDocenteMouseClicked
         // TODO add your handling code here:
         try {
-            docenteControl.setDocente(docenteControl.getListaDocentes().getInfo(tblDocente.getSelectedRow()));
+            Integer indiceDocente = Utiles.encontrarPosicion("docente", modelo.getDocentes().getInfo(tblDocente.getSelectedRow()).getId());
+            docenteControl.setDocente(docenteControl.getListaDocentes().getInfo(indiceDocente));
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
         txtApellido.setText(docenteControl.getDocente().getApellido());
         txtNombre.setText(docenteControl.getDocente().getNombre());
         txtDni.setText(docenteControl.getDocente().getDni());
-        txtTitulo.setText(docenteControl.getDocente().getTitulo());        
+        txtTitulo.setText(docenteControl.getDocente().getTitulo());
     }//GEN-LAST:event_tblDocenteMouseClicked
+
+    private void btnOrdenarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnOrdenarActionPerformed
+        // TODO add your handling code here:
+        try {
+            ordenar();
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_btnOrdenarActionPerformed
+
+    private void txtBusquedaKeyPressed(java.awt.event.KeyEvent evt) {//GEN-FIRST:event_txtBusquedaKeyPressed
+        // TODO add your handling code here:
+        try {
+            buscar();
+        } catch (Exception e) {
+        }
+    }//GEN-LAST:event_txtBusquedaKeyPressed
 
     /**
      * @param args the command line arguments
@@ -347,11 +418,11 @@ public class frmDocente extends javax.swing.JFrame {
     private javax.swing.JButton btnCancelar;
     private javax.swing.JButton btnGuardar;
     private javax.swing.JButton btnModificar;
+    private javax.swing.JButton btnOrdenar;
     private javax.swing.JButton btnSalir;
+    private javax.swing.JRadioButton btnTipoOrden;
     private javax.swing.JComboBox<String> cbxCriterioBusqueda;
     private javax.swing.JComboBox<String> cbxCriterioOrden;
-    private javax.swing.JButton jButton5;
-    private javax.swing.JButton jButton6;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
